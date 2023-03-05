fb1321216426f475fdfa07866d6709d992d8569c
alwaystiys <365825701@qq.com>
2023-03-05 18:45:02 +0800

    add normal color

--- src/main.cpp
+++ src/main.cpp
@@ -105,9 +105,172 @@ void outputRaytracingSimpleImage() {
     }
 }
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+     
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto discriminant = b * b - 4 * a * c;
+    return discriminant > 0;
+}
 
 
+void outputRaySphere() {
+    // Canvas
+    const auto aspect_ratio = 16 / 9;
+    int image_width = 1000;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // Camera and Viewport
+
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * aspect_ratio;
+
+    // the distance between camera and viewports
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+
+    updateImageSize(image_width, image_height);
+
+    for (int i = 0; i < imageSize.x; i++) {
+        for (int j = 0; j < imageSize.y; j++) {
+            auto u = double(i) / (imageSize.x - 1);
+            auto v = double(j) / (imageSize.y - 1);
+
+            // lower_left_corner + u * horizontal + v * vertical 视口上的点
+            // lower_left_corner + u * horizontal + v * vertical - origin 由原点指向视口上的点的向量
+            ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+                
+            // calc hit sphere
+            color c;
+            if (hit_sphere(point3(0, 0, -2), 0.5, r)) {
+                c = color(1.0, 0.0, 0.0);
+            }
+            else {
+                vec3 unit_direction = unit_vector(r.direction());
+                auto t = 0.5 * (unit_direction.y() + 1.0);
+                c = (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+            }
+
+            int ir = static_cast<int>(255.999 * c.x());
+            int ig = static_cast<int>(255.999 * c.y());
+            int ib = static_cast<int>(255.999 * c.z());
+
+            int index = i + j * imageSize.y;
+            pixels[index * 4] = ir;
+            pixels[index * 4 + 1] = ig;
+            pixels[index * 4 + 2] = ib;
+            pixels[index * 4 + 3] = 255;
+        }
+    }
+}
+
+/*
+    COLOR NORMAL
+    Updataed 2023.03.05
+*/
+double hit_sphere_ver2(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+
+    auto discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0.0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+    return discriminant > 0;
+}
+
+double hit_sphere_simple_ver2(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+
+    auto a = dot(r.direction(), r.direction());
+    //auto b = 2 * dot(oc, r.direction());
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+
+    auto discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0.0) {
+        return -1.0;
+    }
+    else {
+        return (-half_b - sqrt(discriminant)) / (2.0 * a);
+    }
+    return discriminant > 0;
+}
+
+
+void outputRayColorNormalSphere() {
+    // Canvas
+    const auto aspect_ratio = 16 / 9;
+    int image_width = 1000;
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera and Viewport
+
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * aspect_ratio;
+
+    // the distance between camera and viewports
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+
+    updateImageSize(image_width, image_height);
+
+    for (int i = 0; i < imageSize.x; i++) {
+        for (int j = 0; j < imageSize.y; j++) {
+            auto u = double(i) / (imageSize.x - 1);
+            auto v = double(j) / (imageSize.y - 1);
+
+            // lower_left_corner + u * horizontal + v * vertical 视口上的点
+            // lower_left_corner + u * horizontal + v * vertical - origin 由原点指向视口上的点的向量
+            ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+
+            // calc hit sphere
+            color c;
+            auto t = hit_sphere_simple_ver2(point3(0, 0, -2), 0.5, r);
+            if (t > 0.0) {
+                c = color(1.0, 0.0, 0.0);
+                vec3 normal = unit_vector(r.at(t) - point3(0, 0, -2));
+                c = 0.5 * color(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
+            }
+            else {
+                vec3 unit_direction = unit_vector(r.direction());
+                auto t = 0.5 * (unit_direction.y() + 1.0);
+                c = (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+            }
+
+            int ir = static_cast<int>(255.999 * c.x());
+            int ig = static_cast<int>(255.999 * c.y());
+            int ib = static_cast<int>(255.999 * c.z());
+
+            int index = i + j * imageSize.y;
+            pixels[index * 4] = ir;
+            pixels[index * 4 + 1] = ig;
+            pixels[index * 4 + 2] = ib;
+            pixels[index * 4 + 3] = 255;
+        }
+    }
+}
 
 int main(void)
 {
@@ -152,8 +315,8 @@ int main(void)
     const char* items[] = { 
         "SimpleImage", 
         "RaySimpleImage", 
-        "Cherry", 
-        "Kiwi", 
+        "RaySphere", 
+        "RayColorNormalSphere", 
         "Mango",
         "Orange", 
         "Pineapple",
@@ -212,6 +375,12 @@ int main(void)
                 break;
             case 1:
                 outputRaytracingSimpleImage();
+                break;
+            case 2:
+                outputRaySphere();
+                break;
+            case 3:
+                outputRayColorNormalSphere();
                 break;
             default:
                 break;
